6f0036f37a688e296c7451720436fb12070350ee
dowhileNodes <77521013+dowhileNodes@users.noreply.github.com>
2021-05-27 23:21:39 +0800

    Registration with firebase

---
+++ app/src/main/java/com/example/mentalhealthsupport/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.mentalhealthsupport;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    private TextView show_date_text;
+    private Button show_calendar;
+    Spinner spinnerStatus;
+
+    EditText et_fullname, et_email, et_password, et_Cnumber;
+    TextView mlogin;
+    CheckBox cb_termsandcondition;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
+    Button signupbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        show_date_text = findViewById(R.id.show_date_text);
+        spinnerStatus = findViewById(R.id.spinnerStatus);
+
+        et_fullname = findViewById(R.id.et_fullname);
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
+        et_Cnumber = findViewById(R.id.et_Cnumber);
+        mlogin = findViewById(R.id.mlogin);
+        cb_termsandcondition = findViewById(R.id.cb_termsandcondition);
+        progressBar = findViewById(R.id.progressBar);
+        signupbtn = findViewById(R.id.signupbtn);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        if (fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),Homepage.class));
+            finish();
+        }
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = et_email.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    et_email.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    et_password.setError("Password is Required");
+                    return;
+                }
+                if (password.length() < 8){
+                    et_password.setError("Password must be 8 characters long");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Homepage.class));
+                        }else {
+                            Toast.makeText(MainActivity.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        String status = spinnerStatus.getSelectedItem().toString();
+        findViewById(R.id.show_calendar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+    }
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+    String date = "month/day/year:" + month + "/" + dayOfMonth + "/" + year;
+    show_date_text.setText(date);
+    }
+}
